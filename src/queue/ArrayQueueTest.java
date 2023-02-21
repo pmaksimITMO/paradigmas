2eca2deef9fd0aa46af5cd7a023d2ee725ff930c
Dns56 <podgorodnev64@gmail.com>
2023-02-21 12:52:22 +0300

    + hw 3 base version with contracts and tests

---
+++ src/queue/ArrayQueueTest.java
@@ -0,0 +1,40 @@
+package queue;
+
+import base.Selector;
+import base.TestCounter;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+//import static queue.Queues.*;
+
+/**
+ * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
+ */
+public final class ArrayQueueTest {
+    public static final Selector SELECTOR = new Selector(ArrayQueueTest.class)
+            .variant("Base", variant(Queues.QueueModel.class, d -> () -> d))
+            ;
+
+    private ArrayQueueTest() {
+    }
+
+    public static void main(final String... args) {
+        SELECTOR.main(args);
+    }
+
+    /* package-private */ static <M extends Queues.QueueModel, T extends Queues.QueueChecker<M>> Consumer<TestCounter> variant(
+            final Class<M> type,
+            final T tester,
+            final Queues.Splitter<M> splitter
+    ) {
+        return new ArrayQueueTester<>(type, tester, splitter)::test;
+    }
+
+    /* package-private */ static <M extends Queues.QueueModel, T extends Queues.QueueChecker<M>> Consumer<TestCounter> variant(
+            final Class<M> type,
+            final T tester
+    ) {
+        return variant(type, tester, (t, q, r) -> List.of());
+    }
+}
